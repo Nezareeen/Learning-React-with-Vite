ca6e9bea59119709dea6a6e5373605744ba0747e
Nezareeen <nezareeen@gmail.com>
2025-07-05 21:49:41 +0530

    Taiwan

---
+++ learning-react/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Taiwan from './components/Greet'
+import Welcome from './components/Welcome'
+
+function App() {
+  
+
+  return (
+    <><div className='app-container'>
+      <Taiwan/>
+      <Welcome/>
+      </div>
+    </>
+  )
+}
+
+export default App
